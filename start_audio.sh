c3d9b0fbcac582bdf241ab6c90eaa47bdde96838
mak-simgithub <info@mak-sim.ch>
2026-05-08 20:59:21 +0200

    systemd kills stuff after 90sec

--- start_audio.sh
+++ start_audio.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Ensure ALSA is ready
+# Wait for ALSA
 until amixer -c 1 info >/dev/null 2>&1; do
     sleep 0.2
 done
 
-# Set gain low → high to force reinit
+# Force ADC init
 amixer -c 1 sset ADC 10db
 sleep 0.2
 amixer -c 1 sset ADC 40db
 sleep 0.2
 
-# Start Pd
-/usr/bin/pd -nogui /home/pi/repos/mic2led/basic.pd
+# Start Python in background
+/usr/bin/python3 /home/pi/repos/mic2led/udp2gpio.py &
+
+# Start Pd in foreground (main process)
+exec /usr/bin/pd -nogui /home/pi/repos/mic2led/basic.pd
